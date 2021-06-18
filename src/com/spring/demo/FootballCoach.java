dd8c9805f2775757b47dd8ff69c61f166f6baf57
john2994 <slither1010@hotmail.com>
2021-06-18 09:18:53 +0300

    init project

---
+++ src/com/spring/demo/FootballCoach.java
@@ -0,0 +1,10 @@
+package com.spring.demo;
+
+public class FootballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Hit the ball";
+	}
+	
+}
---
+++ src/com/spring/demo/MyApp.java
@@ -0,0 +1,10 @@
+package com.spring.demo;
+
+public class MyApp {
+	public static void main(String[] args) {
+		//create object
+		Coach theCoach = new TrackCoach();
+		//use object
+		System.out.println(theCoach.getDailyWorkout());
+	}
+}
