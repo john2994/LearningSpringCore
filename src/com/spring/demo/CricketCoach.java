e0c4fbda230cb7a107b94b0c6bfe9d4d4cbf77ce
john2994 <slither1010@hotmail.com>
2021-06-20 09:55:22 +0300

    Literal injection

--- src/com/spring/demo/CricketCoach.java
+++ src/com/spring/demo/CricketCoach.java
@@ -3,11 +3,31 @@ package com.spring.demo;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
+	//Setter for emailAddress
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	//Getter for emailAddress
+	public String getEmailAddress() {
+		return emailAddress;
+	}
 	
+	//Setter for team
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	//Getter for team
+	public String getTeam() {
+		return team;
+	}
+	
+	//No args constractor
 	public CricketCoach() {
 		System.out.println("CricketCoach class: no arg const");
 	}
-	
+	//Setter for FortuneService
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CricketCoach class: setter method - setFortuneService");
 		this.fortuneService = fortuneService;
