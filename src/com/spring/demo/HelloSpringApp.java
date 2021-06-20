34fc7c2b8438e65e7b52d05d1045693c442b87cc
john2994 <slither1010@hotmail.com>
2021-06-20 11:28:08 +0300

    HomeWork #3

--- src/com/spring/demo/HelloSpringApp.java
+++ src/com/spring/demo/HelloSpringApp.java
@@ -9,9 +9,12 @@ public class HelloSpringApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		// retrieve bean from spring container
 		Coach theCoach = context.getBean("FootballCoach", Coach.class);
+		Coach secondCoach = context.getBean("FootballCoach", Coach.class);
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
+
+		System.out.println("\ntheCoach:  " + theCoach + "\nsecondCoach:  " + secondCoach);
 		// close the context
 		context.close();
 	}
